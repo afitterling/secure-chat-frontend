c052546c559fe45fc9d9d9eb5a8be95cc4f46610
Alexander Fitterling <info@sp33c.de>
2020-07-05 15:20:06 +0800

    semantic ui

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Input } from 'semantic-ui-react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useParams,
+  Link
+} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App className="App" />
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +22,50 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/channel/:channelId">
+            <Channel />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Channel(probs) {
+  let { channelId } = useParams();
+  return (
+    <div>
+      <h2>Requested channel ID: {channelId}</h2>
+      <Input focus placeholder='Search...' />
+    </div>
+  );
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
 serviceWorker.unregister();
