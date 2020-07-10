47914314d2c5f75e15466b7a36539b8cff3326fa
Alexander Fitterling <info@sp33c.de>
2020-07-10 11:43:48 +0800

    settings

--- src/settings.js
+++ src/settings.js
@@ -1,2 +1,3 @@
-export const API_URL = 'http://localhost:8000/api/';
+export const API_URL = 'https://postbox-api1.sp33c.tech/api/';
+//export const API_URL = 'http://localhost:8000/api/';
 export const MEDIUM_ARTICLE = 'https://medium.com/@sp33c/secure-messaging-603b97621e3d';
