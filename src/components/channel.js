c0c51cf49367de691d20973bdf6a6f17efe80fc3
Alexander Fitterling <info@sp33c.de>
2020-07-11 22:27:19 +0800

    legend

--- src/components/channel.js
+++ src/components/channel.js
@@ -130,7 +130,7 @@ function StatusBar({ settings: { subscribers, encryption } }) {
       <div className="ui message">
         <div className="header">Explanation</div>
         <span><i className="user secret icon"></i> how many listeners (spies and old connections included)</span><br />
-        <span><i className="database icon"></i> <em>on</em>: server / node side in-memory persistency <em>off</em>: no persistency - messages are transitional</span><br />
+        <span><i className="database icon"></i> <em>on</em>: cluster in-memory (RAM) persistency (TTL); <em>off</em>: messages aren't stored anywhere only on screen</span><br />
         <span><i className="lock icon"></i> <em>locked</em>: end to end encryption (PKI) on client and service's node's side.</span><br />
         <span><i className="unlock icon"></i> <em>unlocked</em>: SSL/TLS transportation encryption only; messages can be read by everyone.</span><br />
      <span><a href={MEDIUM_ARTICLE}>See how it all works</a>.</span>
