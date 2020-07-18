1eb5edd1d16e59d876da14e5d5c351adde1a3d7a
Alexander Fitterling <info@sp33c.de>
2020-07-18 16:29:08 +0800

    refactor with working encryption but no ui

---
+++ src/components/status-header.js
@@ -0,0 +1,22 @@
+
+// function StatusBar({ settings: { subscribers, encryption } }) {
+//   encryption = false
+//   return (
+//     <Container className="ui label">
+//       <div className="ui message">
+//         <div className="header"></div>
+//         {/* <span><i className="user secret icon"></i><em>{subscribers}</em></span>&nbsp; */}
+//         <span><i>Message's time to live in-memory (TTL)</i>: <em>12h</em></span>&nbsp;
+//       </div>
+//       <div className="ui message">
+//         <div className="header">Explanation</div>
+//         <span><i className="user secret icon"></i> how many listeners (spies and old connections included)</span><br />
+//         <span><i className="database icon"></i> <em>on</em>: cluster in-memory (RAM) persistency (TTL); <em>off</em>: messages aren't stored anywhere only on screen</span><br />
+//         <span><i className="lock icon"></i> <em>locked</em>: end to end encryption (PKI) on client and service's node's side.</span><br />
+//         <span><i className="unlock icon"></i> <em>unlocked</em>: SSL/TLS transportation encryption only; messages can be read by everyone.</span><br />
+//      <span><a href={MEDIUM_ARTICLE}>See how it all works</a>.</span>
+//       </div>
+//     </Container>
+//   );
+// }
+
---
+++ src/components/text-message.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import {
+  useState,
+} from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { Container, Form  } from 'semantic-ui-react'
+import {
+  postMessage,
+} from '../services/messages';
+import Crypto from '../services/crypto';
+
+export function TextMessageInput({ user, channelId, avatarUrl, onSettingsTransmit }) {
+
+  const [inputMessage, setInputMessage] = useState('');
+  const [persistency, setPersistence] = useState(true);
+  const [encoded, setEncoded] = useState(false);
+
+  const paddingBottom = {
+    marginBottom: '2.0rem'
+  }
+
+  const onchange = (event) => {
+    setInputMessage(event.target.value);
+  }
+
+  const encoderFn = async (string) => {
+    console.log(Crypto.pubKeys);
+    // import 
+    const msg = { 
+      text: 'encrypted message',
+      encoded: {}
+    };
+    let data = {};
+    for (const keyRec in Crypto.pubKeys) {
+      console.log(keyRec);      
+      const encrypted = await Crypto.encryptMessage(Crypto.pubKeys[keyRec], string);      
+      data[keyRec] = arrayBufferToBase64(encrypted);
+//      data[keyRec] = window.btoa(escape(encrypted));
+    }
+    msg.encoded = data;
+    return msg;
+  }
+
+  const postMessageFn = async (content) => {
+    console.log('send content', content);
+    return await postMessage(channelId, {
+      id: uuidv4(),
+      user: user,
+      avatarUrl: avatarUrl,
+      encoded: encoded ? 1 : 0,
+      content: content,
+      persistency: persistency ? 1 : 0,
+      cryptoIsAvailable: crypto.isAvailable ? 1 : 0
+    })
+  }
+
+  const onSubmit = async () => {
+    const textContent = encoded ? await encoderFn(inputMessage) : { text: inputMessage };
+    console.log(await postMessageFn(textContent));
+    setInputMessage('');
+  }
+
+  function onPersistency() {
+    setPersistence(!persistency);
+  }
+
+  const textReducer = (enc, trans) => {
+    return (enc ? 'encrypted' : '') + (enc && trans ? ' ' : '') + (trans ? 'transient' : '') + ' message';
+  }
+
+  function placeholderMessage(){
+    return textReducer(encoded, !persistency);
+  }
+
+  const onDecodeEncode = () => {
+    setEncoded(!encoded);
+  }
+
+  const onPublishKey = () => {
+    console.log(JSON.stringify(Crypto.exportedKeys[0]));
+    postMessageFn(
+      {
+        text: 'key published',
+        key: window.btoa(JSON.stringify(Crypto.exportedKeys[0]))
+      }
+      );
+  }
+
+  return (
+    <Container style={paddingBottom}>
+      <button type="button" className="ui button" onClick={onPublishKey}>publish pub key</button>
+      <Form onSubmit={onSubmit}>
+        <Form.Field required>
+          <div className="ui action input">
+            <button type="button" className="ui icon button" onClick={onPersistency}>
+              <i className="database icon" style={{ 'color': persistency ? 'black' : 'grey' }}></i>
+            </button>
+            <button type="button" className="ui icon button" onClick={onDecodeEncode}>
+              <i className={encoded ? "lock icon": "unlock icon"} style={{ 'color': encoded ? 'black' : 'grey' }}></i>
+            </button>
+            <input value={inputMessage} onChange={onchange}
+                   placeholder={placeholderMessage()}  />
+            <button className="ui button" type="submit">
+              send
+            </button>
+          </div>        
+        </Form.Field>
+      </Form>
+    </Container>
+  );
+}
+
+function arrayBufferToBase64( buffer ) {
+	var binary = '';
+	var bytes = new Uint8Array( buffer );
+	var len = bytes.byteLength;
+	for (var i = 0; i < len; i++) {
+		binary += String.fromCharCode( bytes[ i ] );
+	}
+	return window.btoa( binary );
+}
