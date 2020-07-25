931f4df8c019f601a17c4539ff01e031281fd046
Alexander Fitterling <info@sp33c.de>
2020-07-25 13:59:07 +0800

    url share text

--- src/components/share-url.js
+++ src/components/share-url.js
@@ -22,7 +22,7 @@ export function ClipboardShare(){
       <Header as='h4' onClick={share}>
       <Icon name='copy' color={copied ? 'blue': 'black'}/>
         <Header.Content>
-          {copied ? 'copied' : 'Copy Url to Clipboard'}
+          {copied ? 'copied' : 'Copy URL to Clipboard for sharing'}
         </Header.Content>
       </Header>
     );
